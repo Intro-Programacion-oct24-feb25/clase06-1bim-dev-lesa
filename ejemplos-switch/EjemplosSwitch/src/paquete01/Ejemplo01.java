e703098c424b3ed36f50295de4f13c293cf9a2de
unknown <luiseduardosa2006@gmail.com>
2024-11-14 10:35:32 -0500

    eñe

--- ejemplos-switch/EjemplosSwitch/src/paquete01/Ejemplo01.java
+++ ejemplos-switch/EjemplosSwitch/src/paquete01/Ejemplo01.java
@@ -25,23 +25,25 @@ public class Ejemplo01 {
         
         switch(cadena){
             case "Lunes":
-                System.out.printf("%s", cadena);
+            case "lunes":
+                System.out.printf("%s\n", cadena.toUpperCase());
                 break;
             
             case "Martes":
             case "martes":
-                System.out.printf("%s", cadena);
+                System.out.printf("%s\n", cadena.toUpperCase());
                 break;
             
             case "Viernes":
             case "VIERNES":
             case "viernes":
-                System.out.printf("%s", cadena);
+                System.out.printf("%s\n", cadena.toUpperCase());
                 break;
             
             case "Domingo":
-                System.out.printf("%s, es el séptimo día "
-                        + "de la semana", cadena);
+            case "domingo":
+                System.out.printf("%s\n, es el séptimo día "
+                        + "de la semana", cadena.toUpperCase());
                 break;
                 
             default:
